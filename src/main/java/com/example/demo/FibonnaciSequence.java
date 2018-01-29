897e1b1fc5b0e39afb0a707f3c4f659180a347a1
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-29 11:38:01 -0500

    got it working the way I wanted

--- src/main/java/com/example/demo/FibonnaciSequence.java
+++ src/main/java/com/example/demo/FibonnaciSequence.java
@@ -3,21 +3,24 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 public class FibonnaciSequence {
+    /*This is my code using arrayList
+    In this coding the first 4 numbers can be different.
+    */
     ArrayList<Integer> fibonnaci = new ArrayList<>();
     public ArrayList<Integer> fibonnaciScanner(Integer input_number){
-        Integer n=4;
+        Integer n=3;
         fibonnaci.add(input_number-3);
         fibonnaci.add(input_number-2);
         fibonnaci.add(input_number-1);
         fibonnaci.add(input_number-0);
-        /*
         while (fibonnaci.get(n) <100000){
-            fibonnaci.add(fibonnaci.get(n-1)+fibonnaci.get(n-2)+fibonnaci.get(n-3)
-                    +fibonnaci.get(n-4));
+            fibonnaci.add(fibonnaci.get(n)+fibonnaci.get(n-1)+fibonnaci.get(n-2)
+                    +fibonnaci.get(n-3));
+            n = n+1;
 
-        }*/
+        }
 
-        /*
+        /* //This is my code using a regular array
         int[] fibonnaci=new int[20];
         fibonnaci[n-1]=input_number;
         fibonnaci[n-2]=input_number-1;
